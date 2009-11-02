b5feae734f14657e618deba841af0f740ed21958
Gavin L. Simpson <gavin.simpson@ucl.ac.uk>
2009-11-02 00:00:00 +0000

    version 0.6-21

--- R/wa.R
+++ R/wa.R
@@ -12,7 +12,7 @@
     env <- as.numeric(env)
     ## drop species with no information
     if(any(csum <- colSums(x) == 0))
-    x <- x[, !csum, drop = FALSE]
+        x <- x[, !csum, drop = FALSE]
     ## sample summaries
     n.samp <- nrow(x)
     n.spp <- ncol(x)
@@ -27,51 +27,26 @@
     if(missing(na.tol))
         na.tol <- "min"
     na.tol <- match.arg(na.tol)
-    ## first missing tolerances
-    if(any(NA.TOL <- is.na(tol))) {
-        tol[NA.TOL] <- switch(na.tol,
-                              min = min(tol, na.rm = TRUE),
-                              mean = mean(tol, na.rm = TRUE),
-                              max = max(tol, na.rm = TRUE))
+    if(missing(small.tol))
+        small.tol <- "min"
+    small.tol <- match.arg(small.tol)
+    if(small.tol == "fraction") {
+        if(!(f > 0 && f < 1))
+            stop("'f' must be 0 < f < 1")
+        frac <- f * diff(range(env))
+        if(frac < min.tol)
+            warning("Requested fraction of gradient is < minimum tolerance.")
     }
-    ## second, replace tol < min.tol
-    if(!is.null(min.tol) && any(MIN.TOL <- tol < min.tol)) {
-        if(missing(small.tol))
-            small.tol <- "min"
-        small.tol <- match.arg(small.tol)
-        ## min.tol must be in or on extremesof range(env)
-        if(min.tol < min(env) || min.tol > max(env))
-            stop("'min.tol' must be >= min(env) and <= max(env)")
-        ## warn if "fraction" and f * diff(range(env))) < min.tol
-        if(small.tol == "fraction") {
-            if(!(f > 0 && f < 1))
-                stop("'f' must be 0 < f < 1")
-            frac <- f * diff(range(env))
-            if(frac < min.tol)
-                warning("Requested fraction of gradient is < minimum tolerance.")
-        }
-        tol[MIN.TOL] <- switch(small.tol,
-                               fraction = frac,
-                               absolute = min.tol,
-                               min = min(tol[tol >= min.tol], na.rm = TRUE))
-    }
+    tol <- fixUpTol(tol, na.tol = na.tol, small.tol = small.tol,
+                    min.tol = min.tol, f = f, env = env)
     ## calculate WA estimate of env for each site
     if(tol.dw) {
-        tol2 <- tol^2
-        wa.env <- rowSums(sweep(sweep(x, 2, wa.optima, "*",
-                                      check.margin = FALSE),
-                                2, tol2, "/", check.margin = FALSE)) /
-                                    rowSums(sweep(x, 2, tol2, "/",
-                                                  check.margin = FALSE))
+        wa.env <- WATpred(x, wa.optima, tol, n.samp, n.spp)
     } else {
-        wa.env <- ((x %*% wa.optima) / rowSums(x))[,1, drop = TRUE]
+        wa.env <- WApred(x, wa.optima)
     }
     ## taken averages twice so deshrink
-    expanded <- switch(deshrink,
-                       inverse = inv.deshrink(env, wa.env),
-                       classical = class.deshrink(env, wa.env),
-                       expanded = expand.deshrink(env, wa.env),
-                       none = no.deshrink(env, wa.env))
+    expanded <- deshrink(env, wa.env, type = deshrink)
     wa.env <- expanded$env
     coefficients <- coef(expanded)
     ## site/sample names need to be reapplied
@@ -103,7 +78,13 @@
                 n.samp = n.samp, n.spp = n.spp,
                 deshrink = deshrink, tol.dw = tol.dw,
                 call = .call,
-                orig.x = x, orig.env = env)
+                orig.x = x, orig.env = env,
+                options.tol =
+                list(useN2 = useN2,
+                     na.tol = na.tol,
+                     small.tol = small.tol,
+                     min.tol = min.tol,
+                     f = f))
     class(res) <- "wa"
     return(res)
 }
